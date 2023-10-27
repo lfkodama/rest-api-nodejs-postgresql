a0b96426fba9ae5744163ed6157922df850319bd
Fernando Kodama <lfkodama@gmail.com>
2023-10-26 22:20:40 -0300

    Store data into json file

--- controllers/products.js
+++ controllers/products.js
@@ -16,13 +16,15 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+    const products = Product.fetchAll(products => {   //products é a função de callback
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
     });
+    
 };

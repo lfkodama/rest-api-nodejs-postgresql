382176dc6aa70f31f77688b9b6a24dd0cf2afa30
Fernando Kodama <lfkodama@gmail.com>
2023-11-18 17:35:51 -0300

    Add Product Form Writing into products.json

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,8 +9,13 @@ exports.getAddProduct = (req, res, next) => {
         activeAddProduct: true});
     };
 
+
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 };
@@ -20,7 +25,7 @@ exports.getProducts = (req, res, next) => {
         res.render('admin/products', {
             prods: products,
             pageTitle: 'Admin Products',
-            path: '/admin/products'
+            path: 'admin/products'
         });
     });
-}
+};

44ae3ee0cff12172a823edd195f02dcf94fcd353
Fernando Kodama <lfkodama@gmail.com>
2023-11-22 21:50:42 -0300

    Accessing product by ID

--- models/product.js
+++ models/product.js
@@ -24,6 +24,7 @@ module.exports = class Product {
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             fs.readFile(p, (err, fileContent) => {
                 products.push(this);

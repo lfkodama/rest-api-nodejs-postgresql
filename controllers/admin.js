e59997d595d7ab5f6c52f97435c8f7d1a84c0f95
Fernando Kodama <lfkodama@gmail.com>
2023-11-30 21:22:21 -0300

    Populating the Edit Product Page with Data

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,8 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add Product',
-        path: '/admin/add-product'});
+        path: '/admin/add-product',
+        editing: false});
     };
 
 
@@ -20,15 +21,25 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
     const editMode = req.query.edit;
-    if(!editMode) {
+    if (!editMode) {
         return res.redirect('/');
     }
-    res.render('admin/edit-product', {
-        pageTitle: 'Add Product',
-        path: '/admin/edit-product',
-        editing: editMode
 
+    const prodId = req.params.productId;
+
+    Product.findById(prodId, product => {
+        if(!product) {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Add Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+    
+        });
     });
+    
 };
 
 

2fdb219c6b266bd04cafbbdbc23f8512e91449bd
Fernando Kodama <lfkodama@gmail.com>
2024-01-17 21:46:33 -0300

    Creating a dummy user

--- app.js
+++ app.js
@@ -19,6 +19,17 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => {
+        console.log(err);
+    });
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -29,13 +40,24 @@ app.use(errorController.get404);
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
 
-sequelize.sync({ force: true })
+sequelize
+    //.sync({ force: true })
+    .sync()
     .then(result => {
         //console.log(result);
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if(!user) {
+            return User.create({name: 'Max', email: 'max@test.com'});
+        }
+        return user;
+    })        
+    .then(user => {
+        console.log(user);
         app.listen(3000, () => {
             console.log('Server running on port 3000');
         });
-        
     })
     .catch(err => {
         console.log(err);
